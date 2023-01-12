a484752bd250aec10f425ca4ce9cbf77d27aa3ed
David Pilato <david@pilato.fr>
2023-01-12 22:03:29 +0100

    Add the application

---
+++ src/main/java/fr/pilato/example/doc/App.java
@@ -0,0 +1,13 @@
+package fr.pilato.example.doc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
